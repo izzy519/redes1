dbf69ea525b61610a24573c3babf141ee6d125c8
LuisGit <luisfernandocamargopicon@gmail.com>
2024-09-18 09:39:34 +0200

    a otros sitios

--- conectividad.ps1
+++ conectividad.ps1
@@ -1,2 +1,2 @@
-$servers = @("10.57.104.216", "google.com", "4.4.4.4"
+$servers = @("10.57.104.216", "bing.com", "4.4.4.4"
 foreach ($server in $servers) {Test-Connection -ComputerName $server -Count 2 }
